07ee005ca11b462777d06e68549f1854b10f3ae9
Bartosz Kałużny <b.kaluzny.dev@gmail.com>
2024-11-07 16:47:27 +0100

    Fix attr_safe effects in Config class. Add docstring for Config.

--- src/config.py
+++ src/config.py
@@ -5,6 +5,13 @@ from attribute import Attribute
 from exceptions.variable_not_implemented import VariableNotImplementedError
 
 class Config:
+    """
+    Base class for configurations like `Style` and `Content`.
+
+    While making a subclass, define `attributes` list to control possible attributes.
+
+    If you want to skip error checking, pass `attr_safe = False` to `modify()` (constructor) method.
+    """
     attributes: list[Attribute]
     
     def __init__(self, **kwargs) -> None:
@@ -13,18 +20,24 @@ class Config:
     def modify(self, attr_safe=True, **kwargs) -> None:
         attr_names = [attr.name for attr in self.attributes]
 
+        if not attr_safe:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+            return
+
         for key in kwargs.keys():
-            if key not in attr_names and attr_safe:
+            if key not in attr_names:
                 raise AttributeError(f"Attribute {key} is not recognized.")
         
         for attribute in self.attributes:
             value = kwargs.get(attribute.name, attribute.default)
 
-            if value is None and attr_safe:
+            if value is None:
                 raise VariableNotImplementedError(
-                    f"Argument {attribute.name} was not passed for config modification and does not have a default value."
+                    f"Attribute {attribute.name} was not passed for " \
+                    "config modification and does not have a default value."
                 )
-            if not isinstance(value, attribute.attr_type) and attr_safe:
+            if not isinstance(value, attribute.attr_type):
                 raise TypeError(f"Attribute {key} has a value of a wrong type.")
 
             setattr(self, attribute.name, value)
