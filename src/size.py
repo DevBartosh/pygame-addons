5194065df48c9950e898db8b0e799272f9166ca0
Bartosz Kałużny <b.kaluzny.dev@gmail.com>
2024-11-11 18:28:38 +0100

    Add get_tuple() to Size class.

--- src/size.py
+++ src/size.py
@@ -5,3 +5,6 @@ class Size:
     def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
+    
+    def get_tuple(self) -> tuple[int, int]:
+        return (self.width, self.height)
