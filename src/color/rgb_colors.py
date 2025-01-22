11933bd402078a9b6864f876650632674f29b678
Bartosz Kałużny <b.kaluzny.dev@gmail.com>
2025-01-22 15:07:01 +0100

    Add pale colors, reorganize rgb_colors file

--- src/color/rgb_colors.py
+++ src/color/rgb_colors.py
@@ -1,27 +1,38 @@
 from pygame import Color
 
-BLACK = Color(0, 0, 0)
 WHITE = Color(255, 255, 255)
-RED = Color(255, 0, 0)
-GREEN = Color(0, 255, 0)
+LIGHT_GRAY = Color(200, 200, 200)
+DARK_GRAY = Color(128, 128, 128)
+BLACK = Color(0, 0, 0)
+
 BLUE = Color(0, 0, 255)
-YELLOW = Color(255, 255, 0)
-MAGENTA = Color(255, 0, 255)
+DARK_BLUE = Color(0, 0, 100)
+PALE_BLUE = Color(200, 230, 250)
 AQUA = Color(0, 255, 255)
+DARK_AQUA = Color(0, 100, 100)
+
+LIME = Color(130, 170, 50)
+GREEN = Color(0, 255, 0)
+DARK_GREEN = Color(0, 100, 0)
+PALE_GREEN = Color(200, 250, 230)
 
 ORANGE = Color(255, 120, 0)
-PURPLE = Color(128, 0, 128)
-LIME = Color(109, 168, 50)
+PALE_ORANGE = Color(255, 230, 200)
 BROWN = Color(120, 60, 0)
-PINK = Color(255, 120, 215)
+DARK_BROWN = Color(55, 30, 0)
 
-DARK_GRAY = Color(128, 128, 128)
-LIGHT_GRAY = Color(200, 200, 200)
+PINK = Color(255, 120, 215)
+MAGENTA = Color(255, 0, 255)
+PURPLE = Color(128, 0, 128)
+DARK_PURPLE = Color(74, 0, 74)
+PALE_PURPLE = Color(230, 195, 250)
+
+RED = Color(255, 0, 0)
 DARK_RED = Color(100, 0, 0)
-DARK_GREEN = Color(0, 100, 0)
-DARK_BLUE = Color(0, 0, 100)
+PALE_RED = Color(255, 190, 185)
+
+YELLOW = Color(255, 255, 0)
 DARK_YELLOW = Color(100, 100, 0)
-DARK_AQUA = Color(0, 100, 100)
-DARK_BROWN = Color(75, 40, 0)
+PALE_YELLOW = Color(230, 255, 190)
 
 TRANSPARENT = Color(0, 0, 0, 0)
