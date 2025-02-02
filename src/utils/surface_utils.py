cc95829a3c2796fa24353c2b2b3f8d7d755f0297
Bartosz Kałużny <b.kaluzny.dev@gmail.com>
2025-02-02 13:31:13 +0100

    Add represent function for Surface

--- src/utils/surface_utils.py
+++ src/utils/surface_utils.py
@@ -1,5 +1,6 @@
 import numpy
 import pygame
+from cryptography.hazmat.primitives.hashes import SHA256, Hash
 
 class SurfaceUtils:
     @staticmethod
@@ -11,3 +12,12 @@ class SurfaceUtils:
         surf_2_array = pygame.surfarray.array2d(surface_2)
 
         return numpy.array_equal(surf_1_array, surf_2_array)
+    
+    @staticmethod
+    def str_surface(surface: pygame.Surface) -> str:
+        surf_array = pygame.surfarray.array2d(surface)
+        surface_hash = Hash(SHA256())
+        surface_hash.update(str(surf_array).encode())
+        string = f"<Surface(sha256={hex(int.from_bytes(surface_hash.finalize()))}>"
+        
+        return string
