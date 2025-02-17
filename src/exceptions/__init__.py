be1de4965187bfe599c43c23c126333529891423
Bartosz Kałużny <b.kaluzny.dev@gmail.com>
2025-02-17 12:07:19 +0100

    Add comment in exceptions module

--- src/exceptions/__init__.py
+++ src/exceptions/__init__.py
@@ -1,2 +1,6 @@
+"""
+Module containing exeptions used in library.
+"""
+
 from .attribute_duplicated_error import AttributeDuplicatedError
 from .variable_not_implemented import VariableNotImplementedError
